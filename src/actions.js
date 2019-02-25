6b3729353c86f97fbcb4c9d29136fb3edf95acbd
Pete Kreppein <pmkreppein@gmail.com>
2019-02-24 19:06:39 -0500

    remove key for DS

--- src/actions.js
+++ src/actions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export function fetchWeather(){
   return function(dispatch){
-      axios.get(" https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/51d643056c7eff52da442bdd51163010/40.7128,-74.0060?exclude=minutely")
+      axios.get(" https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast//40.7128,-74.0060?exclude=minutely")
       .then((response) => {
         dispatch({type: 'GET_WEATHER', payload: response.data })
       })
